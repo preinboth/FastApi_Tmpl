26d51631ed04391a2f3398ba77882531c772e721
Peter Reinboth <github@preinboth.de>
2025-02-03 14:37:45 +0100

    test black

--- format.bat
+++ format.bat
@@ -1,3 +1,20 @@
-black .
-isort .
-flake8 .
+black src\.
+rem Überprüfen der Ausgabe von black
+if errorlevel 1 (
+    echo Fehler bei black
+    exit /b 1
+)
+
+isort src\.
+rem Überprüfen der Ausgabe von isort
+if errorlevel 1 (
+    echo Fehler bei isort
+    exit /b 1
+)
+
+flake8 src\.
+rem Überprüfen der Ausgabe von flake8
+if errorlevel 1 (
+    echo Fehler bei flake8
+    exit /b 1
+)
